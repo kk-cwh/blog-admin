a42ecc62f33e8f45239959a4418663bb4c3773d6
jaak <zhangykvip@126.com>
2018-04-04 18:21:11 +0800

    添加

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,5 @@
 import HelloWorld from '@/components/HelloWorld'
 import Main from '@/views/Main.vue'
-import Blog from '@/blog-view/index.vue'
 
 // 不作为Main组件的子页面展示的页面单独写，如下
 export const loginRouter = {
---
+++ src/views/articles/preview.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+<div class="nav-bar">
+  
+</div>
+
+</div>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+<style lang="less" scoped>
+
+</style>
+
